6086b5a77b9b636bc69888a6bfa74ca64f8b14b1
Maël LE PETIT <makibeardy@gmail.com>
2022-05-03 13:04:27 -0400

    Create all site pages

---
+++ pages/gallery.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/layouts/Navbar";
+import Footer from "../components/layouts/Footer";
+
+const GalleryPage = () => (
+  <div className="wrapper">
+    <Navbar active_page="Gallery" />
+    <Footer />
+  </div>
+);
+
+export default GalleryPage;
---
+++ pages/mint.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/layouts/Navbar";
+import Footer from "../components/layouts/Footer";
+
+const MintPage = () => (
+  <div className="wrapper">
+    <Navbar active_page="Mint" />
+    <Footer />
+  </div>
+);
+
+export default MintPage;
