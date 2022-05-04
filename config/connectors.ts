5d8cdd812de343ab4deec6572a3390b964dfe293
Maël LE PETIT <makibeardy@gmail.com>
2022-05-04 12:09:42 -0400

    Setup web3 provider

---
+++ config/connectors.ts
@@ -0,0 +1,26 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+
+const POLLING_INTERVAL = 12000
+const RPC_URLS: { [chainId: number]: string } = {
+  1: process.env.RPC_URL_1,
+  4: process.env.RPC_URL_4,
+}
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+})
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: RPC_URLS,
+  chainId: 1,
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+})
+
+export const walletlink = new WalletLinkConnector({
+  url: RPC_URLS[1],
+  appName: 'web3-react example',
+  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001],
+})
