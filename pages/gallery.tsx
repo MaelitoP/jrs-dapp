9a6b21e52e410c3e945f8c96d2dc6ed6af6345c5
Maël LE PETIT <makibeardy@gmail.com>
2022-05-06 17:43:03 -0400

    Fix static props loading

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import Navbar from "../components/layouts/Navbar";
 import Footer from "../components/layouts/Footer";
 
@@ -6,16 +8,14 @@ import Dropdown from "../components/Dropdown";
 import Image from "../components/Image";
 
 const GalleryPage = ({ data }) => {
-  console.log(data);
-
   return (
     <div className="wrapper">
       <Navbar active_page="Gallery" />
 
       <div className="gallery-container py-5">
         <div className="container m-auto ">
-          <div className="grid grid-cols-3 gap-3">
-            <div className="col-span-3">
+          <div className="grid grid-cols-4 gap-3">
+            <div className="col-span-4">
               <h2 className="font-semibold welcome-title text-white text-1lg mr-auto">
                 GALLERY
               </h2>
@@ -25,16 +25,17 @@ const GalleryPage = ({ data }) => {
                 <Dropdown key={name} name={name} attributes={attributes} />
               ))}
             </div>
-            <div className="col-span-2">
+            <div className="gallery col-span-3">
               {data.map((metadata, index) => (
                 <Image
                   key={index}
+                  style={{ marginRight: 5, borderRadius: 8 }}
                   blurDataURL="https://media1.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e4738ueiwkbt9433t8ezfdj9ivu1z0fv188vms7q7vb&rid=giphy.gif&ct=g"
                   placeholder="blur"
                   src={metadata.image}
                   alt={metadata.description}
-                  width="300"
-                  height="300"
+                  width="255"
+                  height="255"
                 />
               ))}
             </div>
@@ -47,12 +48,19 @@ const GalleryPage = ({ data }) => {
   );
 };
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // Fetch data from external API
-  const res = await fetch(
-    `https://bafybeievgx7rdlemudnvgyto5t2wq2mpnlg66blpa3uyqzdat5i5yrumva.ipfs.dweb.link/json/_metadata.json`
+  const res = await axios.get(
+    "https://bafybeievgx7rdlemudnvgyto5t2wq2mpnlg66blpa3uyqzdat5i5yrumva.ipfs.dweb.link/json/_metadata.json",
+    {
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "User-Agent": "*",
+      },
+    }
   );
-  const data = await res.json();
+
+  const data = res.data;
   // Pass data to the page via props
   return { props: { data } };
 }
