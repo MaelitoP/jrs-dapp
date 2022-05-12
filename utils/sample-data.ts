b256555b5af8c36e060c0761f4b0853839598687
Maël LE PETIT <makibeardy@gmail.com>
2022-05-12 10:46:59 -0400

    Add Bandanas & other attributes to filter feature

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -67,19 +67,88 @@ export const sampleNFTData: Attributes[] = [
       {
         name: 'White metal tooth',
       },
+      {
+        name: 'Metal',
+      },
+      {
+        name: 'Cracked 1',
+      },
+      {
+        name: 'Cracked 2',
+      },
+      {
+        name: 'Yellow',
+      },
+      {
+        name: 'White red glow',
+      },
+      {
+        name: 'White blue glow',
+      },
+      {
+        name: 'White diamond',
+      },
+      {
+        name: 'Gold',
+      },
     ],
   },
   {
     name: 'Eyes',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'Base purple',
       },
       {
-        name: 'Trait 2',
+        name: 'Base  blue',
       },
       {
-        name: 'Trait 3',
+        name: 'Base brown',
+      },
+      {
+        name: 'Base green',
+      },
+      {
+        name: 'Base red',
+      },
+      {
+        name: 'Base gold',
+      },
+      {
+        name: 'Snake green',
+      },
+      {
+        name: 'Snake red',
+      },
+      {
+        name: 'Snake gold',
+      },
+      {
+        name: '8 Ball',
+      },
+      {
+        name: 'Jrs icon',
+      },
+      {
+        name: 'Target yellow',
+      },
+      {
+        name: 'Target orange',
+      },
+      {
+        name: 'Target green',
+      },
+      {
+        name: 'Jrs coins',
+      },
+      {
+        name: 'Diamonds blue',
+      },
+      {
+        name: 'Diamonds red',
+      },
+      {
+        name: 'Diamonds pink',
       },
     ],
   },
@@ -87,13 +156,46 @@ export const sampleNFTData: Attributes[] = [
     name: 'Bandanas',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'White fabric',
       },
       {
-        name: 'Trait 2',
+        name: 'White fabric jrs',
       },
       {
-        name: 'Trait 3',
+        name: 'Grey fabric',
+      },
+      {
+        name: 'Black fabric',
+      },
+      {
+        name: 'Black fabric jrs',
+      },
+      {
+        name: 'Red fabric',
+      },
+      {
+        name: 'Purple fabric',
+      },
+      {
+        name: 'Orange fabric',
+      },
+      {
+        name: 'Brown leather',
+      },
+      {
+        name: 'Brown leather jrs',
+      },
+      {
+        name: 'Black leather',
+      },
+      {
+        name: 'Black leather jrs 1',
+      },
+      {
+        name: 'Black leather jrs 2',
+      },
+      {
+        name: 'Black leather jrs 3',
       },
     ],
   },
@@ -101,13 +203,64 @@ export const sampleNFTData: Attributes[] = [
     name: 'Eye Cover',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'Patch 1 brown',
       },
       {
-        name: 'Trait 2',
+        name: 'Patch 1 black',
       },
       {
-        name: 'Trait 3',
+        name: 'Patch 1 black blue gems',
+      },
+      {
+        name: 'Patch 1 black jrs 1',
+      },
+      {
+        name: 'Patch 1 black jrs 2',
+      },
+      {
+        name: 'Patch 2 rusted',
+      },
+      {
+        name: 'Patch 2 silver',
+      },
+      {
+        name: 'Patch 2 copper',
+      },
+      {
+        name: 'Patch 2 gold',
+      },
+      {
+        name: 'Patch 3 rusted',
+      },
+      {
+        name: 'Patch 3 silver',
+      },
+      {
+        name: 'Patch 3 copper',
+      },
+      {
+        name: 'Patch 3 gold',
+      },
+      {
+        name: 'Patch 4 black',
+      },
+      {
+        name: 'Patch 4 brown 1',
+      },
+      {
+        name: 'Patch 4 brown 2',
+      },
+      {
+        name: 'Glasses copper',
+      },
+      {
+        name: 'Glasses copper green',
+      },
+      {
+        name: 'Glasses black tinted',
+      },
+      {
+        name: 'Glasses gold',
       },
     ],
   },
@@ -143,13 +296,106 @@ export const sampleNFTData: Attributes[] = [
     name: 'Hats',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'Pirate 1 brown 1',
       },
       {
-        name: 'Trait 2',
+        name: 'Pirate 1 brown 1 jrs',
       },
       {
-        name: 'Trait 3',
+        name: 'Pirate 1 brown 2',
+      },
+      {
+        name: 'Pirate 1 black',
+      },
+      {
+        name: 'Pirate 1 black jrs',
+      },
+      {
+        name: 'Pirate 2 black',
+      },
+      {
+        name: 'Pirate 2 brown 1',
+      },
+      {
+        name: 'Pirate 2 brown 2',
+      },
+      {
+        name: 'Pirate 3 black',
+      },
+      {
+        name: 'Pirate 3 dirty jrs',
+      },
+      {
+        name: 'Pirate 3 brown',
+      },
+      {
+        name: 'Pirate 3 brown jrs coin',
+      },
+      {
+        name: 'Pirate 3 2-tones',
+      },
+      {
+        name: 'Pirate 3 wanted',
+      },
+      {
+        name: 'Privateer fabric navy gold jrs',
+      },
+      {
+        name: 'Privateer fabric navy gold jrs hair',
+      },
+      {
+        name: 'Privateer fabric black jrs',
+      },
+      {
+        name: 'Privateer fabric black jrs hair',
+      },
+      {
+        name: 'Privateer leather black hair',
+      },
+      {
+        name: 'Privateer leather black',
+      },
+      {
+        name: 'Privateer leather brown',
+      },
+      {
+        name: 'Samurai black',
+      },
+      {
+        name: 'Samurai black gold',
+      },
+      {
+        name: 'Samurai black red',
+      },
+      {
+        name: 'Viking rusted',
+      },
+      {
+        name: 'Viking rusted black',
+      },
+      {
+        name: 'Viking rusted black red',
+      },
+      {
+        name: 'Viking rusted black green',
+      },
+      {
+        name: 'Viking gold',
+      },
+      {
+        name: 'Trooper rusted green',
+      },
+      {
+        name: 'Trooper rusted black',
+      },
+      {
+        name: 'Trooper black',
+      },
+      {
+        name: 'Trooper gold',
+      },
+      {
+        name: 'Trooper green jrs',
       },
     ],
   },
@@ -157,13 +403,40 @@ export const sampleNFTData: Attributes[] = [
     name: 'In Mouth',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'Cigar',
       },
       {
-        name: 'Trait 2',
+        name: 'Cigario',
       },
       {
-        name: 'Trait 3',
+        name: 'Cigarette',
+      },
+      {
+        name: 'Joint',
+      },
+      {
+        name: 'Pipe brown rusted',
+      },
+      {
+        name: 'Pipe black',
+      },
+      {
+        name: 'Pipe brown red',
+      },
+      {
+        name: 'Pipe brown gold',
+      },
+      {
+        name: 'Pipe gold',
+      },
+      {
+        name: 'Dagger rusted',
+      },
+      {
+        name: 'Dagger black',
+      },
+      {
+        name: 'Dagger gold jrs',
       },
     ],
   },
@@ -171,13 +444,31 @@ export const sampleNFTData: Attributes[] = [
     name: 'Beard',
     attributes: [
       {
-        name: 'Trait 1',
+        name: 'Grey',
       },
       {
-        name: 'Trait 2',
+        name: 'Black',
       },
       {
-        name: 'Trait 3',
+        name: 'White',
+      },
+      {
+        name: 'Pinch grey',
+      },
+      {
+        name: 'Pinch black',
+      },
+      {
+        name: 'Pinch white',
+      },
+      {
+        name: 'Stach grey',
+      },
+      {
+        name: 'Stach black',
+      },
+      {
+        name: 'Stach white',
       },
     ],
   },
